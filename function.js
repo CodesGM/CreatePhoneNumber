dbadbb414b971eb50806463527363f8cdd0b87df
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-09-02 16:19:29 -0400

    Update function.js

--- function.js
+++ function.js
@@ -1,1 +1,10 @@
-const createPhoneNumber = numbers => `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+
+// By template literals:
+const createPhoneNumber1 = numbers => `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+
+
+// By mapping:
+function createPhoneNumber2(numbers){
+  let t = "(***) ***-****";
+  return numbers.map(n=> t = t.replace("*", n)).slice(-1)[0];
+}
